c6309be6ca38b12c7ed1e842122abfe0ef446bb8
AJ Penninga <aj@penninga.net>
2026-04-03 23:22:17 -0400

    Fixed version checker

--- app/Livewire/VersionNotice.php
+++ app/Livewire/VersionNotice.php
@@ -13,19 +13,38 @@ class VersionNotice extends Component
 
     public bool $updateAvailable = false;
 
-    public function mount(VersionChecker $checker): void
+    public function mount(): void
     {
-        $latest = $checker->getLatest();
+        $this->refreshVersion();
+    }
 
-        if ($latest && version_compare($latest['version'], config('logr.version'), '>')) {
-            $this->updateAvailable = true;
-            $this->latestVersion = $latest['version'];
-            $this->releaseUrl = $latest['url'];
-        }
+    public function checkNow(): void
+    {
+        $latest = app(VersionChecker::class)->forceCheck();
+        $this->applyLatest($latest);
     }
 
     public function render()
     {
         return view('livewire.version-notice');
+    }
+
+    protected function refreshVersion(): void
+    {
+        $latest = app(VersionChecker::class)->getLatest();
+        $this->applyLatest($latest);
+    }
+
+    protected function applyLatest(?array $latest): void
+    {
+        if ($latest && version_compare($latest['version'], config('logr.version'), '>')) {
+            $this->updateAvailable = true;
+            $this->latestVersion = $latest['version'];
+            $this->releaseUrl = $latest['url'];
+        } else {
+            $this->updateAvailable = false;
+            $this->latestVersion = null;
+            $this->releaseUrl = null;
+        }
     }
 }
